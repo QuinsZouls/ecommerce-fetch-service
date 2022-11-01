a78a11716323484612e38b82dc973779ed79d29d
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2022-11-01 07:29:30 -0600

    feature: improve data fetching model

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { NODE_ENV, PORT = 3000, LOG_FORMAT = 'dev', LOG_DIR = '/tmp/logs', ORIGIN = '*' } = process.env;
 
 // i18n
 export const LANGUAGES = ['en'];
--- src/services/web.service.ts
+++ src/services/web.service.ts
@@ -1,11 +1,35 @@
-import puppeteer from 'puppeteer';
+import puppeteer, { Page } from 'puppeteer';
 
+async function autoScroll(page: Page) {
+  await page.evaluate(async () => {
+    await new Promise<void>(resolve => {
+      let totalHeight = 0;
+      const distance = 1026;
+      const timer = setInterval(() => {
+        const scrollHeight = document.body.scrollHeight;
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+
+        if (totalHeight >= scrollHeight - window.innerHeight) {
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100);
+    });
+  });
+}
 export default class WebService {
   public async getItemsList(url: string) {
-    const browser = await puppeteer.launch({ headless: true }); // for test disable the headlels mode,
+    const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--incognito', '--disable-setuid-sandbox'] }); // for test disable the headlels mode,
     const page = await browser.newPage();
-    await page.setViewport({ width: 1000, height: 926 });
+    //await page.setViewport({ width: 1800, height: 1026 });
+
+    await page.goto(url, { waitUntil: 'load' });
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    await page.goto(url, { waitUntil: 'networkidle0' });
     await page.goto(url, { waitUntil: 'networkidle2' });
+    await autoScroll(page);
+    await page.waitForSelector('#gallery-layout-container > div:nth-child(12)');
     const items = await page.evaluate(() => {
       const div = document.querySelectorAll('#gallery-layout-container > div');
       const list: any[] = [];
@@ -25,6 +49,7 @@ export default class WebService {
 
       return list;
     });
+    browser.close();
     return items;
   }
 }
